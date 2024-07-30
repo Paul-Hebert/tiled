8f32a67509bf20cd685ce22d3307271e64f05884
Paul Hebert <paul@cloudfour.com>
2024-07-29 20:55:49 -0700

    Add grid component

---
+++ src/components/Grid/Grid.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{ size: number }>();
+
+const scale = 10;
+</script>
+
+<template>
+  <svg :viewBox="`0 0 ${scale * size} ${scale * size}`">
+    <template v-for="i in size + 1">
+      <line
+        :x1="0"
+        :x2="scale * size"
+        :y1="(i - 1) * scale"
+        :y2="(i - 1) * scale"
+      />
+      <line
+        :y1="0"
+        :y2="scale * size"
+        :x1="(i - 1) * scale"
+        :x2="(i - 1) * scale"
+      />
+    </template>
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  overflow: visible;
+}
+line {
+  stroke: #ddd;
+  stroke-width: 1;
+  stroke-linecap: round;
+}
+</style>
