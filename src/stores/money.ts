c07887b8a841cf3324f9558b062d043d340bd13a
Paul Hebert <paul@cloudfour.com>
2024-08-21 19:38:30 -0700

    Money composable

---
+++ src/stores/money.ts
@@ -0,0 +1,31 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useMoney = defineStore("money", () => {
+  const playerMoney = ref(0);
+  const income = ref(0);
+
+  const canAfford = (cost: number) => playerMoney.value >= cost;
+
+  const spend = (cost: number) => {
+    if (!canAfford(cost)) {
+      throw new Error("Insufficient funds");
+    }
+
+    playerMoney.value -= cost;
+  };
+
+  const earn = (amount: number) => (playerMoney.value += amount);
+  const earnIncome = () => (playerMoney.value += income.value);
+  const increaseIncome = (amount: number) => (income.value += amount);
+
+  return {
+    playerMoney,
+    spend,
+    canAfford,
+    earn,
+    income,
+    earnIncome,
+    increaseIncome,
+  };
+});
